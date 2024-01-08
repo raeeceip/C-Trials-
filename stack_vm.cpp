2d2d36868d80b68e3b8213e410d898c7bcf8f60e
raeeceip <chisomchibogu@cmail.carleton.ca>
2024-01-07 23:36:43 -0500

    adding functions for tetch and decode

--- stack_vm.cpp
+++ stack_vm.cpp
@@ -15,6 +15,11 @@ header format
 3 - Undefined
 
 */
+
+/*
+Reference : Phillip Bohun
+
+*/
 // functions
 
 // reserveing memory
@@ -27,4 +32,22 @@ i32 StackVM::getType(i32 instruction)
     i32 type = 0xc0000000;
     type = (type & instruction) >> 30;
     return type;
+}
+
+i32 StackVM::getData(i32 instruction)
+{
+    i32 data = 0x3fffffff;
+    data = data & instruction;
+    return data;
+}
+
+void StackVM::fetch()
+{
+    pc++;
+}
+
+void StackVM::decode()
+{
+    typ = getType(memory[pc]);
+    dat = getData(memory[pc]);
 }
